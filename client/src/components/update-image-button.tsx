f44b3d85b67920d78c86dbc51be4cfb9783430aa
Kazmania21 <91277965+Kazmania21@users.noreply.github.com>
2025-07-29 16:53:26 -0400

    Fixed build

--- client/src/components/update-image-button.tsx
+++ client/src/components/update-image-button.tsx
@@ -1,11 +1,4 @@
 import React, { useContext, useRef } from 'react';
-import ContentDiv from '../components/content-div.tsx';
-import EditableText from '../components/editable-text.tsx';
-import DeleteButton from '../components/delete-button.tsx';
-import ProjectTechnologies from '../components/project-technologies.tsx';
-import ProjectUrls from '../components/project-urls.tsx';
-import ProjectTags from '../components/project-tags.tsx';
-import { IProject } from '../types/project';
 import { CrudContext } from '../components/crud-provider.tsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencil } from '@fortawesome/free-solid-svg-icons';
@@ -17,12 +10,11 @@ interface UpdateImageButtonProps {
 
 const UpdateImageButton: React.FC<UpdateImageButtonProps> = ({projectId, className=""}) => {
   //console.log(project);
-  const apiUrl = import.meta.env.VITE_API_URL;
   const inputRef = useRef<HTMLInputElement>(null);
   const { endpoints } = useContext(CrudContext);
   const projects = endpoints["projects"];
 
-  const handleFileChange = async (e: React.ChangeEvent<HTNLInputElement>) => {
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
